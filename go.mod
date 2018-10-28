c3d455488e7976a4f7abdb0dcb075ac3a73710b8
Patrick Tsai <patrick@simplypatrick.com>
2018-10-27 23:44:56 -0700

    Initial working sample code

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/p47t/md2cfl
+
+require (
+	github.com/kentaro-m/blackfriday-confluence v0.0.0-20180726151043-3a37b25b4ee2
+	github.com/p47t/blackfriday-confluence v0.0.0-20181028064106-1109f0059169
+	github.com/russross/blackfriday/v2 v2.0.1
+)
--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	bfconfluence "github.com/p47t/blackfriday-confluence"
+	bf "github.com/russross/blackfriday/v2"
+)
+
 func main() {
+	renderer := &bfconfluence.Renderer{}
+	extensions := bf.CommonExtensions
+	md := bf.New(bf.WithRenderer(renderer), bf.WithExtensions(extensions))
+	input := "# sample text" // # sample text
+	ast := md.Parse([]byte(input))
+	output := renderer.Render(ast) // h1. sample text
+	fmt.Printf("%s\n", output)
 }
